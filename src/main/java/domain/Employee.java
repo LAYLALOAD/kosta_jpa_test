5d45549db3ad47f1410718efb9c11e21e1894529
LAYLALOAD <yjkim7885@knou.ac.kr>
2023-11-03 16:53:43 +0900

    231103

--- src/main/java/domain/Employee.java
+++ src/main/java/domain/Employee.java
@@ -1,21 +1,38 @@
 package domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name="employee")
 public class Employee {
   @Id
-  @Column(name="emp_id")
+  @Column(name="emp_id", length = 6)
   private String empId;
-  @Column(name="emp_name")
+  @Column(name="emp_name", nullable = false, length = 10)
   private String empName;
-  @Column(name="dept_id")
-  private int deptId;
-  @Column(name="join_date")
+//  @Column(name="dept_id")
+//  private int deptId;
+  @ManyToOne
+  @JoinColumn(name="dept_id")
+  private Department department;
+  @Enumerated(EnumType.STRING)
+  private EmpType empType;
+  @Column(name="join_date", nullable = false, length = 10)
   private String joinDate;
+  @Column(nullable = false)
   private Long salary;
+
+  public Employee() {
+  }
+
+  public Employee(String empId, String empName, Department department, EmpType empType, String joinDate, Long salary) {
+    this.empId = empId;
+    this.empName = empName;
+    this.department = department;
+    this.joinDate = joinDate;
+    this.salary = salary;
+    this.empType = empType;
+  }
 
   public void setEmpId(String empId) {
     this.empId = empId;
@@ -33,12 +50,12 @@ public class Employee {
     this.empName = empName;
   }
 
-  public int getDeptId() {
-    return deptId;
+  public Department getDepartment() {
+    return department;
   }
 
-  public void setDeptId(int deptId) {
-    this.deptId = deptId;
+  public void setDepartment(Department department) {
+    this.department = department;
   }
 
   public String getJoinDate() {
@@ -55,5 +72,13 @@ public class Employee {
 
   public void setSalary(Long salary) {
     this.salary = salary;
+  }
+
+  public EmpType getEmpType() {
+    return empType;
+  }
+
+  public void setEmpType(EmpType empType) {
+    this.empType = empType;
   }
 }
